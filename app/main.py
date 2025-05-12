bec141ab1493811054bc642124cfc5de7834d5d7
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-05-12 23:00:15 +0200

    second

--- app/main.py
+++ app/main.py
@@ -13,7 +13,9 @@ def create_person_list(people: list) -> list:
         new_list.append(new)
     for pers in people:
         if pers.get("husband", None):
-                Person.people[pers["name"]].husband = Person.people[pers["husband"]]
+            Person.people[pers["name"]].husband = Person.people[pers["husband"]]
+            Person.people[pers["husband"]].wife = Person.people[pers["name"]]
         elif pers.get("wife", None):
             Person.people[pers["name"]].wife = Person.people[pers["wife"]]
+            Person.people[pers["wife"]].husband = Person.people[pers["name"]]
     return new_list
